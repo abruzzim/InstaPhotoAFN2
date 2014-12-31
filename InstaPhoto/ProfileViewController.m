c1cdd27c1fece5bc13dc135fd861a216f7c80786
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-12-30 23:29:31 -0500

    Add Complete Profile View Items

--- InstaPhoto/ProfileViewController.m
+++ InstaPhoto/ProfileViewController.m
@@ -15,11 +15,15 @@
 @implementation ProfileViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // Custom view controller behavior.
     //self.title                = @"Profile";
     //self.tabBarItem.image     = [UIImage imageNamed:@"Profile"];
     self.view.backgroundColor = [UIColor yellowColor];
+    
+    /* Add Picture */
     
     // Create an image view object to display an image.
     UIImageView *myView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Me.jpg"]];
@@ -28,8 +32,40 @@
     // in its superview's coordinate system.
     myView.frame = CGRectMake(25, 30, 100, 160);
     
-    // Add the profile view as a subview.
+    // Add the picture as a subview.
     [self.view addSubview:myView];
+    
+    /* Add Name */
+    
+    // Initialize and return a newly allocated view object with the specified frame rectangle.
+    UILabel *nameLbl = [[UILabel alloc] initWithFrame:CGRectMake(30, 200, 280, 40)];
+    // Set the text property.
+    nameLbl.text = @"Name: Mario Abruzzi";
+    // Add the label as a subview.
+    [self.view addSubview:nameLbl];
+    
+    /* Add City */
+
+    UILabel *cityLbl = [[UILabel alloc] initWithFrame:CGRectMake(30, 240, 280, 40)];
+    cityLbl.text = @"From: New York";
+    [self.view addSubview:cityLbl];
+    
+    /* Add Biography */
+
+    UITextView *bioText = [[UITextView alloc] initWithFrame:CGRectMake(30, 280, 150, 400)];
+    // Set the text font.
+    bioText.font = [UIFont fontWithName:@"Helvetica" size:15];
+    // Boolean value indicating that this receiver is not editable.
+    bioText.editable = NO;
+    // Add text.
+    bioText.text = @"Bio: Mario Abruzzi is coding along with Gregg Pollack, the founder of Envy Labs and teacher of multiple courses at Code School. Code School teaches web programming in the comfort of my browser with video lessons, coding chalenges and screencasts.";
+    [self.view addSubview:bioText];
+
+    /* Add Membership */
+    
+    UILabel *memberSinceLbl = [[UILabel alloc] initWithFrame:CGRectMake(30, 700, 280, 40)];
+    memberSinceLbl.text = @"Member Since: 1997";
+    [self.view addSubview:memberSinceLbl];
     
 }
 
