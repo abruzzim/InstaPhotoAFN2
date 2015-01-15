23b7c95e8adf0d6b1ebf73160773d1d07bbb3b48
Mario Abruzzi <abruzzim@framework-solutions.com>
2015-01-14 21:36:00 -0500

    Add UIImage (Logo) to Favorites View

--- InstaPhoto/FavoritesViewController.m
+++ InstaPhoto/FavoritesViewController.m
@@ -31,6 +31,26 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor orangeColor];
     NSLog(@"%%FavoritesViewController-I-DEBUG, 'backgroundColor' property set.");
+    
+    /* Setup Logo Subview */
+    
+    // Create an image view object to display an image.
+    UIImageView *myLogo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo.jpg"]];
+    NSLog(@"%%FavoritesViewController-I-DEBUG, 'Logo image view instantiated.");
+    
+    // Scale content to fit in the view's frame, while
+    // preserving the original aspect ratio.
+    [myLogo setContentMode:UIViewContentModeCenter];
+    NSLog(@"%%FavoritesViewController-I-DEBUG, Image view content mode set.");
+    
+    // Set the frame, which descibes the view's location and size
+    // in its superview's coordinate system.
+    myLogo.frame = self.view.frame;
+    NSLog(@"%%FavoritesViewController-I-DEBUG, Image view frame set.");
+    
+    // Add the logo image view as a subview.
+    [self.view addSubview:myLogo];
+    NSLog(@"%%FavoritesViewController-I-DEBUG, Subview added.");
 }
 
 - (void)didReceiveMemoryWarning {
